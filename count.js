3916eebc428b9c4750813d25645cf67d1d4f3689
GiorgiGoglidze <giorgigoglidze43@gmail.com>
2022-06-10 17:42:03 +0400

    counter

---
+++ count.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("btn"),
+count=0;
+button.onclick = function(){
+count=count+1;
+button.innerHTML='click'+count;}
+
+var button = document.getElementById("btn1"),
+count=10
+button.onclick = function(){
+count=count-1;
+button.innerHTML='click'+count;}
+
+
+    
